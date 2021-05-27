7eef3a2d7b7884899ebbc05c07f31cbfa24febfd
Shweta2105 <sghadi21@gmail.com>
2021-05-27 23:29:27 +0530

    playing for winning

--- src/snakeAndLadder/snakeAndLadder.java
+++ src/snakeAndLadder/snakeAndLadder.java
@@ -4,34 +4,37 @@ public class snakeAndLadder {
 	public static void main(String[] args) {
 		int player;
 		int position = 0;
-		
-		int dice = (1 + (int)(Math.random() * 6));
-		System.out.println(+dice);
-		
-		int option = (int)((Math.random() * 10) % 3);
+
 		
 		
-		System.out.println(+ option);
-		
-		switch(option)
-		{
-		case 1:
-			position = position +dice;
-			System.out.println("Current position is ladder :"+ position);
-			break;
+		while (position < 100) {
+			int dice = (1 + (int) (Math.random() * 6));
+			int option = (int) ((Math.random() * 10) % 3);
+
 			
-		case 2:
-			if(position > dice) {
-				position = position - dice;
-				System.out.println("Current position is snake :"+position);
-				
+
+			switch (option) {
+			case 1:
+				position = position + dice;
+				System.out.println("Current position is ladder :" + position);
+				break;
+
+			case 2:
+				if (position > dice) {
+					position = position - dice;
+					System.out.println("Current position is snake :" + position);
+
+				} else if (position < dice) {
+					position = 0;
+				}
+				break;
+			default:
+				position = position;
+				System.out.println("Current position is  :" + position);
 			}
-			break;
-		default:
-			position = position;
-			System.out.println("Current position is  :"+position);
+			System.out.println(+option);
+			System.out.println(+dice);
 		}
-		System.out.println(+dice);
 	}
 
 }
