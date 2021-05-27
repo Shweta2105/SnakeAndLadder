b3d7acdf4e75c96d0e68af9641e7a3c7a18c90b9
Shweta2105 <sghadi21@gmail.com>
2021-05-27 23:16:23 +0530

    Added Snake and ladder position

--- src/snakeAndLadder/snakeAndLadder.java
+++ src/snakeAndLadder/snakeAndLadder.java
@@ -6,9 +6,32 @@ public class snakeAndLadder {
 		int position = 0;
 		
 		int dice = (1 + (int)(Math.random() * 6));
+		System.out.println(+dice);
 		
-		System.out.println(dice);
+		int option = (int)((Math.random() * 10) % 3);
 		
+		
+		System.out.println(+ option);
+		
+		switch(option)
+		{
+		case 1:
+			position = position +dice;
+			System.out.println("Current position is ladder :"+ position);
+			break;
+			
+		case 2:
+			if(position > dice) {
+				position = position - dice;
+				System.out.println("Current position is snake :"+position);
+				
+			}
+			break;
+		default:
+			position = position;
+			System.out.println("Current position is  :"+position);
+		}
+		System.out.println(+dice);
 	}
 
 }
