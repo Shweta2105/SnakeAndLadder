4803c7eacca7909cd5c1eb27b62e7abc3e7bac45
Shweta2105 <sghadi21@gmail.com>
2021-05-27 22:51:40 +0530

    Game started with player

---
+++ src/snakeAndLadder/snakeAndLadder.java
@@ -0,0 +1,12 @@
+package snakeAndLadder;
+
+public class snakeAndLadder {
+	public static void main(String[] args) {
+		int player;
+		int position = 0;
+		
+		System.out.println("is at position :"+position);
+		
+	}
+
+}
